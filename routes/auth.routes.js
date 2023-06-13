5de2fe6a3766ab64a02f2a9b299a442c68142dd2
Inês Almeida <ines_almeida_203@hotmail.com>
2023-06-13 19:48:09 +0100

    done

--- routes/auth.routes.js
+++ routes/auth.routes.js
@@ -6,7 +6,10 @@ const router = express.Router();
 
 const User = require('../models/User.model');
 
-router.get("/signup", (req, res, next) => {
+const { isLoggedIn, isLoggedOut } = require('../middleware/route-guard.js');
+
+
+router.get("/signup", isLoggedOut, (req, res, next) => {
   res.render("auth/signup");
 });
 
@@ -27,13 +30,63 @@ router.post("/signup", (req, res, next) => {
 });
  
 
-router.get("/profile/:username", (req, res, next) => {
-    User.findOne({username: req.params.username})
-        .then(foundUser => {
-            console.log('foundUser', foundUser)
-            res.render('auth/profile', foundUser)
-        })
-        .catch(err => console.log(err))
+router.get("/profile", isLoggedIn, (req, res, next) => {
+    if(req.session.currentUser){
+        User.findOne({ username: req.session.currentUser.username })
+         .then(foundUser => {
+             console.log('foundUser', foundUser)
+             res.render('auth/profile', foundUser)
+         })
+         .catch(err => console.log(err))
+     }
+     else{
+       res.render('auth/profile')
+     }
+ });
+
+
+router.get('/login', isLoggedOut, (req, res) => res.render('auth/login'));
+
+router.post('/login', (req, res,next) => {
+    console.log('req.session', req.session)
+
+    const { username, password } = req.body;
+
+    if(username === '' || password === '') {
+        res.render('auth/login', {
+            errorMessage: 'Please enter both, username and password to login'
+        });
+        return;
+    }
+
+    User.findOne({ username })
+        .then(user => {
+            console.log('user', user)
+            if(!user){
+                res.render('auth/login', { errorMessage: 'Username is not registered. Try with other.' });
+                return;
+            } else if (bcrypt.compareSync(password, user.password)) {
+                const { username, email } = user;
+                req.session.currentUser = { username, email };
+                res.render('auth/profile', user );
+              } else {
+                res.render('auth/login', { errorMessage: 'Incorrect password.' });
+              }
+            })
+            .catch(error => next(error));
 });
+
+
+router.post('/logout', isLoggedIn, (req, res, next) => {
+    req.session.destroy(err => {
+      if (err) next(err);
+      res.redirect('/');
+    });
+  });
+
+
+router.get('/main', isLoggedIn, (req, res, next) => res.render('auth/main'));
+
+
 
 module.exports = router;
